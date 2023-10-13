b13e38b5072169944f116ebc6827cf03696da37a
Harun Skender <harun@clutchy.io>
2023-10-13 17:12:02 +0200

    completed - 6. UI

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -5,6 +5,7 @@ import injectedModule from '@web3-onboard/injected-wallets';
 import SendTransaction from './SendTransaction';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { fetchWalletSuccess } from '../store/reducers/walletSlice';
+import { navigate } from './NaiveRouter';
 
 // harun:comment - `injected` enables the app to support browser-injected wallets like MetaMask.
 const injected = injectedModule();
@@ -42,16 +43,20 @@ const Navigation: React.FC = () => {
     <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-ful text-sm py-4 bg-gray-800">
       <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
         <div className="flex items-center justify-between">
-          <a
-            className="flex-none text-xl font-semibold dark:text-white"
-            href="."
+          <div
+            className="flex-none text-xl font-semibold dark:text-white cursor-pointer"
+            onClick={() => {
+              // harun:comment - fix navigation
+              navigate('/transactions');
+            }}
           >
             Transactions List
-          </a>
+          </div>
         </div>
-        <div className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
+        {/* harun:comment - fix connect button */}
+        <div className="hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block pt-3 sm:pt-0">
           <div className="flex flex-col gap-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
-            {!!wallet && (
+            {wallet && (
               <>
                 <SendTransaction />
                 <p className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-gray-200 text-sm">

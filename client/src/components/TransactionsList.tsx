32cfb47c9792654748c46eb190ad0b8c45576f68
Harun Skender <harun@clutchy.io>
2023-10-13 17:21:54 +0200

    completed - 7. Human Readable Values

--- client/src/components/TransactionsList.tsx
+++ client/src/components/TransactionsList.tsx
@@ -1,19 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GetAllTransactions } from '../queries';
-import { Transaction, TransactionsData } from '../types';
+import { TransactionsData } from '../types';
 import { navigate } from './NaiveRouter';
+import { weiToEth } from '../utils';
 
 const TransactionList: React.FC = () => {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  // harun:comment - deleted unnecesarry state as we use useQuery to handle server state
+  const { loading, error, data } =
+    useQuery<TransactionsData>(GetAllTransactions);
 
-  const { loading, error, data } = useQuery<TransactionsData>(GetAllTransactions);
-
-  useEffect(() => {
-    if (data && data.getAllTransactions) {
-      setTransactions(data.getAllTransactions);
-    }
-  }, [data]);
+  // harun:comment - derived state
+  const transactions = data?.getAllTransactions;
 
   const handleNavigate = (hash: string) => navigate(`/transaction/${hash}`);
 
@@ -41,11 +39,17 @@ const TransactionList: React.FC = () => {
     <div className="flex flex-col mt-20">
       <div className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
         <div className="p-1.5 min-w-full inline-block align-middle">
-          {!!transactions.length ? (
+          {!!transactions && !!transactions.length ? (
             <>
               {transactions.map(({ hash, to, from, value }) => (
-                <div key={hash} className="bg-white shadow-sm p-4 md:p-5 border rounded border-gray-300 mt-3 hover:border-blue-500 cursor-pointer" onClick={() => handleNavigate(hash)}>
-                  <span className="font-bold">{value} ETH</span> sent from <span className="font-bold">{from}</span> to <span className="font-bold">{to}</span>
+                <div
+                  key={hash}
+                  className="bg-white shadow-sm p-4 md:p-5 border rounded border-gray-300 mt-3 hover:border-blue-500 cursor-pointer"
+                  onClick={() => handleNavigate(hash)}
+                >
+                  <span className="font-bold">{weiToEth(value)} ETH</span> sent
+                  from <span className="font-bold">{from}</span> to{' '}
+                  <span className="font-bold">{to}</span>
                 </div>
               ))}
             </>
@@ -56,6 +60,6 @@ const TransactionList: React.FC = () => {
       </div>
     </div>
   );
-}
+};
 
 export default TransactionList;
